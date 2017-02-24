58a0fda1e6bccbab1d54d77ddef23a7e92143ee0
NadimRai <rai_nadim@yahoo.com>
2017-02-24 10:23:34 -0800

    added jumbotron

---
+++ src/app/components/pages/foodlog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foodlog',
+  templateUrl: './foodlog.component.html',
+  styleUrls: ['./foodlog.component.css']
+})
+export class FoodlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
