9f19b1d00281030200a2503a1ab596029a2a1c2b
NadimRai <rai_nadim@yahoo.com>
2017-02-24 18:14:15 -0800

    submission of half complete assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
 import {AppComponent} from './app.component';
 
 import {NavbarComponent} from './components/navbar/navbar.component';
@@ -10,7 +10,7 @@ import {FoodlogComponent} from './components/pages/foodlog.component';
 import { routing } from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+  imports:      [ BrowserModule, routing, FormsModule ],
   declarations: [ AppComponent, 
                   NavbarComponent, 
                   JumbotronComponent,
--- src/app/components/pages/foodlog.component.ts
+++ src/app/components/pages/foodlog.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-foodlog',
   templateUrl: './foodlog.component.html',
   styleUrls: ['./foodlog.component.css']
 })
-export class FoodlogComponent implements OnInit {
+export class FoodlogComponent  {
+	
+	 public tasks: Task[] = [
+      new Task("MacDonald Burger","Had two days in a row", 900),
+      new Task("Green Salad"," For Lunch", 200),
+      new Task("Kebab","Didn't drink soda", 600),
+      new Task("Dim Sum","Had to share", 200)
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+   selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
 
+  finishedEditing() {
+  	this.selectedTask = null;
+  }
 }
+
+
+ 
+  
+
+
